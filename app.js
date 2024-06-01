406488c034c007661b2b1ee34ad98346b2a19961
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-06-01 19:20:57 +0530

    add: mongodb connection

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+require('dotenv').config();
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+// mongo db connection
+mongoose
+  .connect(process.env.MONGOURI)
+  .then((msg) => console.log('Connected to DB'))
+  .catch((error) => console.log(error));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -23,12 +32,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
