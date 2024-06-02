2e9d63f32fda58abdb9a15de48961011ffa6e6d9
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-06-02 13:26:21 +0530

    update: users router signup and login

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const userController = require('../controllers/userController');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/signup', userController.signup_get);
+router.post('/signup', userController.signup_post);
+
+router.get('/login', userController.login_get);
+router.post('/login', userController.login_post);
 
 module.exports = router;
