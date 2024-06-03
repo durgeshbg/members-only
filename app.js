50a2aa17e469ab6fe85675af5e5ccace187ead6b
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-06-03 22:26:53 +0530

    update: pass status as local var

--- app.js
+++ app.js
@@ -66,6 +66,7 @@ app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  res.locals.status = err.status || 500;
   // render the error page
   res.status(err.status || 500);
   res.render('error', { user: req.user });
