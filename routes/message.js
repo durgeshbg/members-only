69df7d9631726d83a0519a28208b88c5b41d56cd
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-06-03 18:11:20 +0530

    add: message create get/post routes

--- routes/message.js
+++ routes/message.js
@@ -4,4 +4,7 @@ const messageController = require('../controllers/messageController');
 
 router.get('/', messageController.message_list);
 
+router.get('/create', messageController.message_create_get);
+router.post('/create', messageController.message_create_post);
+
 module.exports = router;
