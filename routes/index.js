f5f76fef1b539b48b80c952c879b3425d24ce400
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-06-03 14:15:02 +0530

    update: index routes

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const indexController = require('../controllers/indexController');
 const { isAuth } = require('./authMiddleware');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express', user: req.user });
-});
+router.get('/', indexController.index);
 
-router.get('/profile', isAuth, (req, res, next) => {
-  res.render('profile', { title: 'Profile', user: req.user });
-});
+router.get('/profile', indexController.profile);
+
+
 
 module.exports = router;
