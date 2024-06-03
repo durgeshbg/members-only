bfdc7d2ff0e16191e271148987c66ebc93e339ea
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-06-03 17:46:23 +0530

    add: joinclub get/post controllers

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,13 +1,44 @@
+const { body, validationResult } = require('express-validator');
+const asyncHandler = require('express-async-handler');
+const User = require('../models/user');
+
 exports.index = (req, res, next) => {
   res.render('index', { title: 'Express', user: req.user });
 };
 
-exports.profile = [
-  isAuth,
-  (req, res, next) => {
-    res.render('profile', { title: 'Profile', user: req.user });
-  },
-];
+exports.profile = (req, res, next) => {
+  res.render('profile', { title: 'Profile', user: req.user });
+};
 
-exports.joinclub_get = () => {};
-exports.joinclub_post = () => {};
+exports.joinclub_get = (req, res) => {
+  res.render('joinclub', { title: 'Join Club', user: req.user });
+};
+
+exports.joinclub_post = [
+  body('key')
+    .trim()
+    .toLowerCase()
+    .custom((key) => {
+      if (key === 'quicksort') return true;
+      return false;
+    })
+    .withMessage('Incorrect key'),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render('joinclub', {
+        title: 'Join Club',
+        key: req.body.key,
+        user: req.user,
+        errors: errors.array(),
+      });
+    } else {
+      try {
+        await User.findByIdAndUpdate(req.body.user_id, { member: true });
+        res.redirect('/profile');
+      } catch (e) {
+        next(e);
+      }
+    }
+  }),
+];
