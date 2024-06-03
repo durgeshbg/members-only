2d73b4b08de55541c019c4f241caf31634825888
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-06-03 18:25:24 +0530

    add: message create get/post controllers

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,5 +1,38 @@
+const { body, validationResult } = require('express-validator');
 const Message = require('../models/message');
+const { isAuth } = require('../routes/authMiddleware');
+const asyncHandler = require('express-async-handler');
 
 exports.message_list = (req, res, next) => {
   res.send('Message list');
 };
+
+exports.message_create_get = [
+  isAuth,
+  (req, res, next) => {
+    res.render('message_form', { title: 'Create Message', user: req.user });
+  },
+];
+exports.message_create_post = [
+  body('title').trim().isLength({ min: 1 }).withMessage('Title required'),
+  body('body').trim().isLength({ min: 10 }).withMessage('Body too short'),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const message = new Message({
+      title: req.body.title,
+      body: req.body.body,
+      user: req.body.user_id,
+    });
+    if (!errors.isEmpty()) {
+      res.render('message_form', {
+        title: 'Create Message',
+        user: req.user,
+        errors: errors.array(),
+        message,
+      });
+    } else {
+      await message.save();
+      res.redirect('/messages');
+    }
+  }),
+];
