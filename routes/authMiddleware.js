f59d3b992b34124302276dae63d7bb40cf54734d
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-06-03 13:48:10 +0530

    fix: content header error

--- routes/authMiddleware.js
+++ routes/authMiddleware.js
@@ -1,13 +1,15 @@
 exports.isAuth = (req, res, next) => {
   if (req.isAuthenticated()) next();
-  res.redirect('/login');
+  else res.redirect('/login');
 };
 exports.isAdmin = (req, res, next) => {
   if (req.isAuthenticated() && req.user.admin) next();
-  res.locals.notAdmin = 'Unauthorired: you are not an admin!';
-  res.status(403);
+  else {
+    res.locals.notAdmin = 'Unauthorired: you are not an admin!';
+    res.status(403);
+  }
 };
 exports.isNotAuth = (req, res, next) => {
   if (req.isAuthenticated()) res.redirect('/profile');
-  next();
+  else next();
 };
