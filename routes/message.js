528e03ef783f254d72ce064e54033a84f34d4ff5
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-06-03 22:01:37 +0530

    add: message delete route

--- routes/message.js
+++ routes/message.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const messageController = require('../controllers/messageController');
+const { isAdmin } = require('./authMiddleware');
 
 router.get('/', messageController.message_list);
 
 router.get('/create', messageController.message_create_get);
 router.post('/create', messageController.message_create_post);
+
+router.post('/delete', isAdmin, messageController.message_delete);
 
 module.exports = router;
