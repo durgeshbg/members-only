3b268cac7385483b12970bfc7217046ef6c68eba
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-06-03 12:23:56 +0530

    add: profile route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express', user: req.user });
 });
 
+router.get('/profile', (req, res, next) => {
+  res.render('profile', { title: 'Profile', user: req.user });
+});
+
 module.exports = router;
